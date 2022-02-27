dae4bf58b46ca261672ea10ac3edeb9c01e7e5b8
Vetrichelvan <pythonhub.py@gmail.com>
2022-02-28 00:55:01 +0530

    Added base editor configuration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,28 @@
-import type { NextPage } from "next";
-import Heading from "../src/Components/Heading";
+import { FC } from "react";
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import { TextStyle } from "@tiptap/extension-text-style";
+import Code from "@tiptap/extension-code";
+import Highlight from "@tiptap/extension-highlight";
+import { Link } from "@tiptap/extension-link";
+import { Color } from "@tiptap/extension-color";
 
-const Home: NextPage = () => {
+const Home: FC = () => {
+	const editor = useEditor({
+		extensions: [
+			StarterKit,
+			TextStyle,
+			Code,
+			Highlight.configure({ multicolor: true }),
+			Link,
+			Color,
+		],
+		content: "<p>Hello World! 🌎️</p>",
+	});
 	return (
-		<>
-			<div>
-				<Heading className={"heading"}>Home Page</Heading>
-			</div>
-		</>
+		<EditorContent editor={editor} />
 	);
 };
+
 
 export default Home;
